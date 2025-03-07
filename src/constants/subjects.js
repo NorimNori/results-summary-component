671cf0ef272efff291cd0dec1c90899d171702ca
Gloria R <norimaqidoe@hotmail.com>
2025-03-07 00:10:12 -0600

    feat: add summaryData array

---
+++ src/constants/subjects.js
@@ -0,0 +1,7 @@
+export const summaryData = [
+    { category: "Reaction", score: 80},
+    { category: "Memory", score: 92},
+    { category: "Verbal", score: 61 },
+    { category: "Visual", score: 73}
+  ];
+  
